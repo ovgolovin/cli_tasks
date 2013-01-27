d94525eee79844b51ab7e52d49b54d53cfd31558
Oleg Golovin <ovgolovin@gmail.com>
2013-01-27 16:28:56 +0400

    task3 update

---
+++ task3/parse_log_c_graph.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+function display_help {
+	echo "Requires 2 parameters."
+	echo "First: name of log file to process."
+	echo "Second: date to make calculations to in format yyyy-mm-dd"
+	echo ""
+	echo "Example usage: parse_log.sh log.txt 2013-01-20"
+}
+
+for parameter in "$@"
+do
+	if [ "$parameter" == "--help" ]
+	then
+		echo "Log plotter"
+		display_help
+		exit
+	fi
+done
+
+
+
+if [ "$#" != 2 ]
+then
+	echo "Wrong number of parameters"
+	display_help
+	exit
+fi
+
+file="$1"
+date="$2"
+
+
+grep  "$date\s12.*\/resume" "$file"| cut -f 1,2,8 | sed -E "s/(.*)ms/\1/"|  awk '{print $1 " " $2 " =resume " $3}' >plot_data~
+grep  "$date\s12.*\/vacancy" "$file"| cut -f 1,2,8 | sed -E "s/(.*)ms/\1/"|  awk '{print $1 " " $2 " =vacancy " $3}' >>plot_data~
+grep  "$date\s12.*\/user" "$file"| cut -f 1,2,8 | sed -E "s/(.*)ms/\1/"|   awk '{print $1 " " $2 " =user " $3}' >>plot_data~
+
+tplot -if plot_data~ -o graph.png -dk dots
+
+rm plot_data~
+
+
+
